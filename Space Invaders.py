8fd18cb1f33db0c10b111cf0262512b93a71132d
Andrew-Muegel <andrew.muegel@gmail.com>
2016-10-04 17:08:22 -0500

    Minor Changes
    Made changes to:
    - colors
    - imports
    - many other minor changes

--- Space Invaders.py
+++ Space Invaders.py
@@ -13,17 +13,8 @@ LOSING/WINNING
 '''
 
 # Import Pygame
-import pygame, sys, time
+import pygame, sys, time, random
 from pygame.locals import *
-from random import randint
-
-# Setup the display of the screen
-pygame.init()
-window = pygame.display.set_mode((1900,985))
-window.fill((0,0,0))
-
-# Set the key repeat
-pygame.key.set_repeat(1,1)
 
 # Define Colors
 WHITE = (255, 255, 255)
@@ -31,6 +22,14 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Setup the display of the screen
+pygame.init()
+window = pygame.display.set_mode((1900,985))
+window.fill(BLACK)
+
+# Set the key repeat
+pygame.key.set_repeat(1,1)
 
 # Set the Ship's Location
 shipLocation=850
@@ -41,23 +40,26 @@ pygame.display.update()
 
 # Main while Loop
 while True:
+    
     # Redraw the Background
     window.blit(background,(1,1))
+    
     # Create the ship rectangle
     pygame.draw.rect(window, (GREEN), (shipLocation,935,50,50),0)
+    
     # Update the Display
     pygame.display.update()
     
     # Checks for events
     for event in pygame.event.get():
-        if event.type==QUIT or (event.type==KEYUP and event.key==K_ESCAPE):
+        if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
             pygame.quit()
             sys.exit()
-        if event.type==KEYDOWN:
-            if event.key==K_LEFT and shipLocation>0:
-                shipLocation=shipLocation-3
-            if event.key==K_RIGHT and shipLocation<1850:
-                shipLocation=shipLocation+3
-            if event.key==K_UP:
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT and shipLocation > 0:
+                shipLocation = shipLocation - 3
+            if event.key == K_RIGHT and shipLocation < 1850:
+                shipLocation = shipLocation + 3
+            if event.key == K_UP:
                 # Shooting Key
                 pass
