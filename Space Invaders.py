f7fe45197147b881253c9fa2883584c207a5d490
TrumpetDude <johnnydollard@gmail.com>
2016-10-05 08:47:01 -0500

    Added Ship
    The ship is 64 pixels tall by 104 pixels wide.

--- Space Invaders.py
+++ Space Invaders.py
@@ -36,7 +36,7 @@ shipLocation=850
 
 #Import Background
 background=pygame.image.load("starBackground.GIF")
-pygame.display.update()
+ship=pygame.image.load("ship.PNG")
 
 # Main while Loop
 while True:
@@ -44,8 +44,8 @@ while True:
     # Redraw the Background
     window.blit(background,(1,1))
     
-    # Create the ship rectangle
-    pygame.draw.rect(window, (GREEN), (shipLocation,935,50,50),0)
+    #Draw the ship
+    window.blit(ship,(shipLocation,921))
     
     # Update the Display
     pygame.display.update()
@@ -58,7 +58,7 @@ while True:
         if event.type == KEYDOWN:
             if event.key == K_LEFT and shipLocation > 0:
                 shipLocation = shipLocation - 3
-            if event.key == K_RIGHT and shipLocation < 1850:
+            if event.key == K_RIGHT and shipLocation < 1796:
                 shipLocation = shipLocation + 3
             if event.key == K_UP:
                 # Shooting Key
